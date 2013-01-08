29f20a94d6a79164db35918e0c9701af48f985db
Michael Anderson <michael.anderson@runwithrobots.com>
2013-01-08 16:15:00 +0800

    Added checking of events JSON to webAPI build tool.

--- webAPI/build.sh
+++ webAPI/build.sh
@@ -2,10 +2,13 @@ echo -e 'RoarAPI - unity (buidling...)'
 echo -e '---------------------'
 
 API_FUNCTION_JSON=./api_functions.json
+EVENT_JSON=./data/events.json
 JSON_LINT=jsonlint
 
 echo -e "[Checking config json]"
 ${JSON_LINT} "${API_FUNCTION_JSON}" > /dev/null
+echo -e "[Checking events json]"
+${JSON_LINT} "${EVENT_JSON}" > /dev/null
 
 echo -e "[Building unity from template]"
 node template.js
