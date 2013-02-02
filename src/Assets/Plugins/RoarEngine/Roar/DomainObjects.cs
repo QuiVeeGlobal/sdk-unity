40d787c5a7b96e136a8e81acbacc3f9a3add954a
Michael Anderson <michael.anderson@runwithrobots.com>
2013-02-02 15:38:27 +0800

    [cleanup] removed cruft from DomainObjects.cs

--- src/Assets/Plugins/RoarEngine/Roar/DomainObjects.cs
+++ src/Assets/Plugins/RoarEngine/Roar/DomainObjects.cs
@@ -566,33 +566,6 @@ namespace Roar
 			public IList<LeaderboardExtraProperties> properties;
 		}
 
-		//TODO: Problem with this is that we only keep one page for each leadeerboard request... we really should merge them somehow.
-		/**
-		 * Expected XML is like this:
-		 *
-		 * <leaderboards>
-		 *   <view status="ok">
-		 *      <ranking ikey="mojo" offset="0" num_results="100" page="1" low_is_high="false">
-		 *         <entry rank="1" player_id="612421456098" value="560">
-		 *           <custom>
-		 *             <property ikey="player_name" value="Monkey"/>
-		 *           </custom>
-		 *         </entry>
-		 *         <entry rank="2" player_id="195104156933" value="514">
-		 *           <custom>
-		 *             <property ikey="player_name" value="Dragon"/>
-		 *           </custom>
-		 *        </entry>
-		 *        <entry rank="3" player_id="440312985759" value="490"/>
-		 *          <custom>
-		 *            <property ikey="player_name" value="Fun and Awesome DUUUUUDE"/>
-		 *           </custom>
-		 *        </entry>
-		 *     </ranking>
-		 *   </view>
-		 * </leaderboards>
-		 */
-
 		public class LeaderboardData
 		{
 			public string board_id;
@@ -983,7 +956,5 @@ namespace Roar
 			}
 		}
 		
-
-
 	}
 }
